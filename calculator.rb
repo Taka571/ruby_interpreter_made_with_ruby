c400c92d503db90e23839b66c48139fa816646e0
Takahiro <tk.iyei@gmail.com>
2020-03-14 22:36:10 +0900

    四則演算インタプリタ

---
+++ calculator.rb
@@ -0,0 +1,34 @@
+require "minruby"
+
+def evaluate(tree)
+  case tree[0]
+  when "lit"
+    tree[1]
+  when "+"
+    left  = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left + right
+  when "-"
+    left  = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left - right
+  when "*"
+    left  = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left * right
+  else
+    # ここでは tree[0] == "/"
+    left  = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left / right
+  end
+end
+
+# ① 計算式の文字列を読み込む
+str = gets
+# ② 計算式の文字列を計算の木に変換する
+tree = minruby_parse(str)
+# ③ 計算の木を実行（計算）する
+answer = evaluate(tree)
+# ④ 計算結果を出力する
+p(answer)
