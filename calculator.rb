61abd6b5e911ee587df7360d4a60d07997c81941
Takahiro <tk.iyei@gmail.com>
2020-03-15 10:37:59 +0900

    比較演算子を追加

--- calculator.rb
+++ calculator.rb
@@ -28,6 +28,30 @@ def evaluate(tree)
     left  = evaluate(tree[1])
     right = evaluate(tree[2])
     left / right
+  when ">"
+    left  = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left > right
+  when "<"
+    left  = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left < right
+  when ">="
+    left  = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left >= right
+  when "<="
+    left  = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left <= right
+  when "=="
+    left  = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left == right
+  when "!="
+    left  = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left != right
   else
     raise "invalid input"
   end
